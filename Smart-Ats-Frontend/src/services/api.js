c56908422ea572f8451810004638c39222e60848
Rajendra Acharya <acharyarajendra48@gmail.com>
2026-06-04 20:22:54 +0530

    final push

--- Smart-Ats-Frontend/src/services/api.js
+++ Smart-Ats-Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://talentflowai-1.onrender.com/api" || "http://localhost:5000/api",
+  baseURL: "https://talentflowai-1.onrender.com/api",
 });
 
 // attach token automatically
