e1989a34c9a5b2a11df61370fbf2e694760be3b6
Rajendra Acharya <acharyarajendra48@gmail.com>
2026-06-06 12:39:11 +0530

    smtp error 6

--- Smart-Ats-Backend/utils/email.js
+++ Smart-Ats-Backend/utils/email.js
@@ -8,14 +8,16 @@ const createTransporter = () => {
   }
 
   return nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
+  family: 4,
+
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
-  connectionTimeout: 30000,
-  greetingTimeout: 30000,
-  socketTimeout: 30000,
 });
 };
 
