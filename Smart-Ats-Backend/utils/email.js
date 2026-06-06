7a9cd753c9abec4016eecb4f045ad87baba36b35
Rajendra Acharya <acharyarajendra48@gmail.com>
2026-06-06 13:18:47 +0530

    after adding brevo 2

--- Smart-Ats-Backend/utils/email.js
+++ Smart-Ats-Backend/utils/email.js
@@ -2,12 +2,14 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT),
+  port: 587,
   secure: false,
-
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
